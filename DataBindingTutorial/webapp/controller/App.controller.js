d08d882930db32039a449933ebb1a63f20bcae27
YuriStanoev <yuri.stanoev@gmail.com>
2025-10-14 22:20:15 +0300

    Data Binding Exercise Steps 9-13

--- DataBindingTutorial/webapp/controller/App.controller.js
+++ DataBindingTutorial/webapp/controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/m/library",
-	"sap/ui/core/mvc/Controller"
-], (mobileLibrary, Controller) => {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/type/Currency"
+], (mobileLibrary, Controller, Currency) => {
 	"use strict";
 
 	return Controller.extend("ui5.databinding.controller.App", {
@@ -9,9 +10,22 @@ sap.ui.define([
 			const oBundle = this.getView().getModel("i18n").getResourceBundle();
 
 			return mobileLibrary.URLHelper.normalizeEmail(
-				`${sFirstName}.${sLastName}@example.com`,
+				sFirstName + "." + sLastName + "@example.com",
 				oBundle.getText("mailSubject", [sFirstName]),
 				oBundle.getText("mailBody"));
+		},
+
+		formatStockValue(fUnitPrice, iStockLevel, sCurrCode) {
+			const oCurrency = new Currency();
+			return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+		},
+
+		onItemSelected(oEvent) {
+			const oSelectedItem = oEvent.getSource();
+			const oContext = oSelectedItem.getBindingContext("products");
+			const sPath = oContext.getPath();
+			const oProductDetailPanel = this.byId("productDetailsPanel");
+			oProductDetailPanel.bindElement({ path: sPath, model: "products" });
 		}
 	});
 });
